ddf16e18b6e11e895bf100b7f2184bd97efec080
AVIJIT GARAI <110466132+AVIJITAG@users.noreply.github.com>
2022-08-18 17:59:27 +0530

    Add files via upload

---
+++ 12-08-2022/AExperssion.java
@@ -0,0 +1,15 @@
+
+
+class AExperssion{
+
+ public static void main(String args[]){
+ 
+ System.out.println("The Given Specified Experssion Is:  ");
+ System.out.println("(25.5 * 3.5 - 3.5 * 3.5)/(40.5 - 4.5)");
+ 
+     double c = (25.5 * 3.5 - 3.5 * 3.5)/(40.5 - 4.5);
+ 
+ System.out.println("Final result =  "  +c);
+
+ }
+}
