8577b68a1e8cf31ffe13aacc43b0a12980d3791c
AVIJIT GARAI <110466132+AVIJITAG@users.noreply.github.com>
2022-09-08 21:26:25 +0530

    Add files via upload

---
+++ 08-09-2022/Check.java
@@ -0,0 +1,23 @@
+//Can you create object of a class from inside the scope of another class constructor//
+
+
+
+
+
+
+package MyPackage;
+
+public class Check {
+
+}
+    public class Question3 {
+    	Question3() {
+    		 Check test = new Check();
+    		 
+    	}
+	        public static void main(String[] args) {
+		  Question3 obj = new Question3();
+
+	}
+
+}
