b61b6081c7edf3a6827e542da20d7c7ba39ace0c
AVIJIT GARAI <110466132+AVIJITAG@users.noreply.github.com>
2022-09-09 15:29:53 +0530

    Add files via upload

---
+++ 08-09-2022/Employee.java
@@ -0,0 +1,26 @@
+package MyPackage;
+
+public class Employee {
+	static void test(int id,String name) {
+	System.out.println("Employee id:" + id+" "+ name);
+	}
+ }
+		
+	
+	
+public class Employeetest {	
+public static void main(String[] args) {
+		
+		Employee.test(10,"avi");
+		Employee.test(20,"jeet");
+		Employee.test(30,"seba");
+		Employee.test(44,"ram");
+		Employee.test(55,"Raju ");
+		
+		
+	}
+}
+ 
+
+
+
