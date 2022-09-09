32361996c8ae4907aaf5294aed3d815e062bb345
AVIJIT GARAI <110466132+AVIJITAG@users.noreply.github.com>
2022-09-09 14:19:56 +0530

    Add files via upload

---
+++ 08-09-2022/java.java
@@ -0,0 +1,16 @@
+/*Develop a public java class and make sure nobody can
+create any object of this class from outside the class */
+
+
+
+
+
+package MyPackage;
+
+ class java {
+   private java () {
+	   
+   }
+     public static void main(String[]args) {
+     }
+     }
