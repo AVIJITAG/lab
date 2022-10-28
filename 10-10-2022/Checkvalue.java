55ba55593f78636b3a6392d088fbcc39fcb96f92
AVIJIT GARAI <110466132+AVIJITAG@users.noreply.github.com>
2022-10-28 09:16:34 +0530

    Update Checkvalue.java

--- 10-10-2022/Checkvalue.java
+++ 10-10-2022/Checkvalue.java
@@ -3,22 +3,22 @@
 
 package Lab10;
 
-public class Checkvalue {
+public class Checkvalue {                                            // create class
 	
-		  public static void main(String[] args) {
+		  public static void main(String[] args) {           // main method
 
 		    int[] num = {1789, 2035 ,1899 ,1456, 2013};
 		    int toFind = 2013;
 		    boolean found = false;
 
-		    for (int n : num) {
+		    for (int n : num) {                           // for loop 
 		      if (n == toFind) {
 		        found = true;
 		        break;
 		      } 
 		    }
 		    
-		    if(found)
+		    if(found)                                   // if statement 
 		      System.out.println(toFind + "  true");
 		    else
 		      System.out.println(toFind + " false ");
